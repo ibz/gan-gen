220ae6254793a065244602c0d3124d2a203b8428
Ioan Bizău <i@ibz.me>
2022-01-21 23:30:55 +0200

    Fix: use proper root directory.

--- stage2/99-gan-requirements/02-run.sh
+++ stage2/99-gan-requirements/02-run.sh
@@ -2,8 +2,8 @@
 
 pip3 install pimoroni-bme280 smbus
 
-git clone https://github.com/ibz/gan.git /home/gan/gan
+git clone https://github.com/ibz/gan.git ${ROOTFS_DIR}/home/gan/gan
 
-echo "0 12 * * * journalctl --vacuum-size=10M" > /etc/cron.d/clean-journal
-chmod 0644 /etc/cron.d/clean-journal && crontab /etc/cron.d/clean-journal
+echo "0 12 * * * journalctl --vacuum-size=10M" > ${ROOTFS_DIR}/etc/cron.d/clean-journal
+chmod 0644 ${ROOTFS_DIR}/etc/cron.d/clean-journal && crontab ${ROOTFS_DIR}/etc/cron.d/clean-journal
 
